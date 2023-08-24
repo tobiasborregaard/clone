52762b05a6878a958726839e9583311ef9156127
tobiasborregaard <75496528+tobiasborregaard@users.noreply.github.com>
2023-08-24 18:26:42 +0200

    231

--- URL_upload/login-app/src/components/dashboard.js
+++ URL_upload/login-app/src/components/dashboard.js
@@ -1,18 +1,18 @@
-// Dashboard.js
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import UploadSite from './Uploadsite.js';
 import ProductList from './ProductList.js';
-import TopNav from './TopNav.js'
+import TopNav from './TopNav.js';
 
 function Dashboard() {
     return (
-        <TopNav/>
+        <div>
+            <TopNav />
             <Routes>
                 <Route path="/dashboard" element={<UploadSite />} />
                 <Route path="/dashboard/products" element={<ProductList />} />
             </Routes>
+        </div>
     );
 }
 
